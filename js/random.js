eebd66d9028a0ae0a380295e38de8e9f9b203592
TeamAbella060420 <jabella@rscm.com.sa>
2022-03-31 11:02:34 +0300

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,15 +1,18 @@
 // Collect input from a user
+const inputLow = prompt('Please provide a number');
 const inputHigh = prompt('Please provide a number');
 
 // Convert the input to a number
+const lowNumber = parseInt(inputLow);
 const highNumber = parseInt(inputHigh);
 
-if (highNumber) {
+
+if (lowNumber && highNumber) {
   // Use Math.random() and the user's number to generate a random number
-  const randomNumber = Math.floor( Math.random() * highNumber ) + 1;
+  const randomNumber = Math.floor( Math.random() * (highNumber - lowNumber + 1 ) ) + lowNumber;
   
   // Create a message displaying the random number
-  console.log(`${randomNumber} is a random number between 1 and ${highNumber}.`);
+  console.log(`${randomNumber} is a random number between ${lowNumber} and ${highNumber}.`);
 } else {
-  console.log('You need to provide a number. Try again.');
+  console.log('You need to provide two numbers. Try again.');
 }
